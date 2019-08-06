a6cfd5f22e5705b1d689a1093af91e90e675a9a6
Nick Gonzalez <gonzaleznick5@gmail.com>
2019-08-06 15:47:03 -0400

    Wheel

--- js/index.js
+++ js/index.js
@@ -1,10 +1,30 @@
 // Your code goes here
-const button = document.querySelector('.destination .btn');
+
+// Mouse Over button
+const button = document.querySelector('.content-pick .destination .btn');
 button.addEventListener('mouseover', (event)=> {
     event.target.style.color = "gray";
-    console.log(`this is my mouse!`);
+    // console.log(`this is my mouse!`);
 
     setTimeout(() => {
         event.target.style.color = "";
     }, 600);
+}, false);
+
+
+//Keydwon 
+
+
+
+
+//Wheel - Class "intro" H2 font size zoom
+
+const funBus = document.querySelector('.intro h2');
+funBus.addEventListener('wheel', (event)=> {
+event.target.style.fontSize = '80px';
+console.log('font zoom!')
+
+setTimeout(() => {
+    event.target.style.fontSize = "";
+}, 600);
 }, false);
